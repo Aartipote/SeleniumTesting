244b03bc553376253e9921fffd948f1ed5f2913b
AartiPote11 <108711181+AartiPote11@users.noreply.github.com>
2023-03-23 17:23:28 +0800

    implicit waits added

--- 01CreatingTestProject/CreditCards.UITests/CreditCardApplicationShould.cs
+++ 01CreatingTestProject/CreditCards.UITests/CreditCardApplicationShould.cs
@@ -2,6 +2,10 @@
 using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using Xunit.Abstractions;
+using System.Drawing.Text;
 
 namespace CreditCards.UITests
 {
@@ -12,6 +16,12 @@ namespace CreditCards.UITests
         private const string ApplyUrl = "http://localhost:5258/Apply";
         private const string PageTitle = "Credit Card Application - Credit Cards";
 
+        private readonly ITestOutputHelper output;
+
+        public CreditCardApplicationShould(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
 
         [Fact]
         public void BeInitiatedFromHomePage_NewLowRate()
@@ -39,14 +49,22 @@ namespace CreditCards.UITests
             using (IWebDriver driver = new ChromeDriver())
             {
                 driver.Navigate().GoToUrl(HomeUrl);
-                DemoHelper.Pause(5000);
+                DemoHelper.Pause();
 
+                //explicity clicking the next buttom on the carousel, can ignore the three lines and use the WebdriverWait to automate the process,
+                //however will take time as it is hardcoded to slide through the carousel slides after 10 secs.
                 IWebElement carouselnext = driver.FindElement(By.CssSelector("[data-slide='next']"));
                 carouselnext.Click();
                 DemoHelper.Pause(1000); //allowing the carousel to load the next page
 
-                IWebElement applyLink = driver.FindElement(By.LinkText("Easy: Apply Now!"));
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+                IWebElement applyLink =
+                    wait.Until((d) => d.FindElement(By.LinkText("Easy: Apply Now!")));
                 applyLink.Click();
+
+             //   IWebElement applyLink = driver.FindElement(By.LinkText("Easy: Apply Now!"));
+               // applyLink.Click();
+                
                 DemoHelper.Pause(5000);
 
             }
@@ -54,21 +72,56 @@ namespace CreditCards.UITests
         }
 
         [Fact]
-        public void BeInitiatedFromHomePage_CustomerService()
+        public void BeInitiatedFromHomePage_EasyApplicaion_Prebuilt_Conditons()
         {
             using (IWebDriver driver = new ChromeDriver())
             {
                 driver.Navigate().GoToUrl(HomeUrl);
                 DemoHelper.Pause(1000);
 
-                IWebElement carouselnext = driver.FindElement(By.CssSelector("[data-slide='next']"));
-                carouselnext.Click();
-                DemoHelper.Pause(1000);
-                carouselnext.Click();       //go to the last silde
-                DemoHelper.Pause(1000);
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(11));
+                //explicitly wait until the element is clickable
+                IWebElement applyLink =
+                    wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Easy: Apply Now!")));
+                applyLink.Click();
 
-                IWebElement applybutton = driver.FindElement(By.ClassName("customer-service-apply-now"));
-                applybutton.Click();
+            }
+        }
+
+
+        [Fact]
+        public void BeInitiatedFromHomePage_CustomerService()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                //Implicit wait code
+                //output.WriteLine($"{DateTime.Now.ToLongTimeString()} Setting implicit wait");
+                //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(38);
+
+                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Navigating to '{HomeUrl}'");
+                driver.Navigate().GoToUrl(HomeUrl);
+
+                //IWebElement carouselnext = driver.FindElement(By.CssSelector("[data-slide='next']"));
+                //carouselnext.Click();
+                //DemoHelper.Pause(1000);
+                //carouselnext.Click();       //go to the last silde
+                //DemoHelper.Pause(1000);   
+
+
+                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Finding Element");
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(35));
+                IWebElement applyLink =
+                    wait.Until((d) => d.FindElement(By.ClassName("customer-service-apply-now")));
+
+
+                //output.WriteLine($"{DateTime.Now.ToLongTimeString()} Finding Element");
+                //IWebElement applyLink = driver.FindElement(By.ClassName("customer-service-apply-now"));
+
+
+                //implicit waits, we can't use complex logic like can with explicit wait.
+                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Element Found Displayed={applyLink.Displayed} Enabled={applyLink.Enabled}");
+                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Clicking Element");
+                applyLink.Click(); 
                 DemoHelper.Pause();
 
                 Assert.Equal(ApplyUrl, driver.Url);
@@ -119,6 +172,7 @@ namespace CreditCards.UITests
     }
 }
 
-//< a class= "btn btn-default" href = "/Apply" > Easy: Apply Now!</a>
-//name="ApplyLowRate"
-//Credit Card Application - Credit CardsCredit Card Application - Credit Cards
+// working with waits:
+// 1st way-using demohelper class to cause that thread to sleep for required period of time. Could use but causes the automation tests to break easily.
+// 2nd way-Explicit waits using WebDriver waits which uses timespan but has a conditional logic
+// 3rd way-Implicit waits using IWebdriver property 
